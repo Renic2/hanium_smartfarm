2942eb638b6a59f08b24f197aa1077af81d4adf0
KangMinKwon <sandwich1202@naver.com>
2025-08-09 15:15:11 +0900

    아두이노_수정

--- Old_ver/Hanium_smartfarm/Arduino_firmware/Arduino/Arduino.ino
+++ Old_ver/Hanium_smartfarm/Arduino_firmware/Arduino/Arduino.ino
@@ -1,6 +1,7 @@
 #include "CareFarm.h"
 
 CareFarm carefarm; // CareFarm 객체 생성
+String cmd;
 
 unsigned long last_sensor_read_time = 0;
 unsigned long last_heartbeat_time = 0;
@@ -16,7 +17,7 @@ void loop() {
     // 2초마다 센서 데이터 읽고 전송
     if (current_time - last_sensor_read_time >= 2000) {
         last_sensor_read_time = current_time;
-        carefarm.readAndSendSensors();
+        carefarm.readSensors();
     }
 
     // 5초마다 Heartbeat 전송
@@ -26,5 +27,5 @@ void loop() {
     }
 
     // 시리얼 명령은 항상 확인
-    carefarm.processSerialCommand();
+    carefarm.processSerialCommand(cmd);
 }
